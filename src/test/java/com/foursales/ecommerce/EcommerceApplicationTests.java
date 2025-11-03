3d5cd28b8a51f8fd1fd09219e52e81c177094be1
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2025-11-03 15:51:04 -0300

    docs: ajuste documentacao

--- src/test/java/com/foursales/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/foursales/ecommerce/EcommerceApplicationTests.java
@@ -1,11 +1,9 @@
 package com.foursales.ecommerce;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class EcommerceApplicationTests {
 
     @Test
