7517486a8503b02fe1074d1f0c06b5c8ce1376e2
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2025-11-03 15:25:52 -0300

    feat: db mysql dump

--- src/main/java/com/foursales/ecommerce/domain/repository/UsuarioRepository.java
+++ src/main/java/com/foursales/ecommerce/domain/repository/UsuarioRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByEmail(String email);
 
